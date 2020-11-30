3e6b5c362a26b9e810039d90f2d30900438c9163
김성민 <evidentkr@naver.com>
2020-11-30 16:58:46 +0900

    불필요 임포트 제거

--- src/test/java/com/rubypaper/PropertiesTest.java
+++ src/test/java/com/rubypaper/PropertiesTest.java
@@ -2,13 +2,10 @@ package com.rubypaper;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-//import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.rubypaper.controller.BoardController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(properties = { "your.name=kkkkk", "your.age=11" })
